7a20fcce84de3aed0c0786574d4d695392369f09
Nguyen Anh Tu <nguyenanhtu230605@gmail.com>
2025-09-23 13:57:30 +0700

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom'
 import AppLayout from './app/layout/AppLayout'
 import AppRoutes from './app/routes/AppRoutes'
@@ -18,5 +17,3 @@ export default function App() {
     </BrowserRouter>
   )
 }
-=======
->>>>>>> 11b6b402ee805b82b9cc0f0cebaccf9615834799
